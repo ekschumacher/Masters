b6f3d6fa982bcbbe8efc99a428eac9d223b6e445
Emily Schumacher <62253972+ekschumacher@users.noreply.github.com>
2020-10-29 14:18:41 -0500

    Update genetic_diversity_visualization_10_29_20.R
    updated genetic diversity

--- genetic_diversity_visualization_10_29_20.R
+++ genetic_diversity_visualization_10_29_20.R
@@ -25,27 +25,76 @@ master_gendiv$Accession <- as.factor(master_gendiv$Accession)
 ne_matrix <- matrix(ncol = 2, nrow = length(master_gendiv[,1]))
 
 ##create new accession names
-ne_matrix <- cbind(paste0(master_gendiv$Species,master_gendiv$Accession), as.numeric(master_gendiv$Ne))
-ne_matrix[,2] <- as.numeric(ne_matrix[,2])
+ne_matrix <- cbind(paste0(master_gendiv$Species,"_",master_gendiv$Accession), as.numeric(master_gendiv$Ne))
+
+##na matrix
+na_matrix <- cbind(paste0(master_gendiv$Species,master_gendiv$Accession), master_gendiv$Na)
+
+##he matrix
+he_matrix <- cbind(paste0(master_gendiv$Species,master_gendiv$Accession), master_gendiv$He)
+
+##
+ho_matrix <- cbind(paste0(master_gendiv$Species,master_gendiv$Accession), master_gendiv$Ho)
+
+#F matrix
+f_matrix <- cbind(paste0(master_gendiv$Species,master_gendiv$Accession), master_gendiv$F)
 
 ##accession list
 accession_list <- unique(ne_matrix[,1])
 
 ##make ne matrix results
-gendiv_df <- matrix(nrow = length(accession_list), ncol = 5)
+ne_df <- matrix(nrow = length(accession_list), ncol = 3)
+
+##colnames(gendiv_df) <- c("Ne","Na","He","Ho","F")
 
 ##write loop to calculate means 
 for(a in 1:length(accession_list)){
   
+  ne_df[a,3] <- mean(as.numeric(ne_matrix[ne_matrix[,1] == paste0(accession_list[[a]]),][,2]))
+  
+  
+}  
+#  gendiv_df[a,2] <- mean(as.numeric(na_matrix[na_matrix[,1] == paste0(accession_list[[a]]),][,2]))
+
+ne_df <- data.frame(ne_df)
+ne_df[,1] <- accession_list
+
+##now separate
+ne_df[,1] <- gsub("\\_.*","",ne_df[,1])
+ne_df[,2] <- gsub("^.*\\_","", ne_df[,1])
+
+
+  
+ # gendiv_df[a,3] <- mean(as.numeric(he_matrix[he_matrix[,1] == paste0(accession_list[[a]]),][,2]))
+  
+ # gendiv_df[a,4] <- mean(as.numeric(ho_matrix[ho_matrix[,1] == paste0(accession_list[[a]]),][,2]))
+  
+#  gendiv_df[a,5] <- mean(as.numeric(f_matrix[f_matrix[,1] == paste0(accession_list[[a]]),][,2]))
+  
+#}
+
+##reformat loop
+
+ne_matrix <- cbind(paste0(master_gendiv$Species,master_gendiv$Accession), as.numeric(master_gendiv$Ne))
+
+
+##write to data frame
+##write loop to calculate means 
+for(a in 1:length(accession_list)){
+  
   gendiv_df[a,1] <- mean(as.numeric(ne_matrix[ne_matrix[,1] == paste0(accession_list[[a]]),][,2]))
-  
-  
-  
+
 }
 
+##create df 
+ne_df <- data.frame(ne_df)
+colnames(ne_df) <- c("Species", "Accession","NE")
 
-ggplot(data = master_gendiv, aes(x=Species, y=Ho)) + 
+ne_df[,2] <- as.factor(ne_df[,2])
+
+###Plot 
+ggplot(data = ne_df, aes(x=Species, y=Ho)) + 
   geom_boxplot(aes(fill=Accession)) + xlab("Species") + 
-  ylab("Expected Heterozygosity") + ggtitle("Expected Heterozygosity") +
+  ylab("Inbreeding Coefficient") + ggtitle("Inbreeding by Species") +
   scale_fill_manual(values = c("gray33", "gray48", "gray77")) 
 
